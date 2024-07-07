799d3100b38a05c6637a91206c4266bbffff93d2
Shashank Suman <103516291+SShanks451@users.noreply.github.com>
2024-07-07 20:31:09 +0530

    Added Exception Handling

---
+++ csharp-advanced/ExceptionHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*
+            try
+            {
+                using (var streamReader = new StreamReader(@"c:\file.zip"))
+                {
+                    var content = streamReader.ReadToEnd();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Soory, an exception occured");
+            }
+            */
+
+            // ----------------------------------------------------------------------------------------------
+
+            try
+            {
+                var api = new YoutubeApi();
+                var videos = api.GetVideos("mosh");    
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ csharp-advanced/ExceptionHandling/YoutubeApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class YoutubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                // Access youtube web service
+                // read the data
+                // create a list of Video objects\
+                throw new Exception("Oops some low level youtube error");
+            }
+            catch (Exception ex)
+            {
+                throw new YoutubeException("Cannot fetch the videos from youtube", ex);
+            }
+
+            return new List<Video>();
+        }
+    }
+}
